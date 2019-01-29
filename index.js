3298ab66263651afe29d5e170c2a2a231ef48643
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2019-01-29 20:37:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,19 +18,6 @@ bot.on('ready',function(){
 });
 //GETS THE BOT ONLINE
 
-//WELCOME/LEAVE MESSAGE
-bot.on('guildMemberAdd', (member) => {
-    const welcomechannel = member.guild.channels.find('name', `:clap:✧･ﾟรายชื่อคนเข้าออก`);
-    welcomechannel.send({embed: new Discord.RichEmbed()
-        .setColor("#FF0000")
-        .setTitle("**New Member**")
-        .setDescription(`:warning:||**Welcome** ${member} to the **Phenomenal Network**.`)
-        .setImage("https://cdn.discordapp.com/attachments/518367175174782983/539750406218055681/Phenomenal.jpg")
-        .setTimestamp()});
-})
-
-//WELCOME/lEAVE MESSAGE
-
 //REGISTRIES
 bot.registry.registerGroup('network', 'Network');
 bot.registry.registerCommandsIn(__dirname + "/commands");
